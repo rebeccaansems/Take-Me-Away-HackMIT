00549ebd5f332fb2181ae780a3582703b8cf0c02
Sid Panjwani <sidpanjwani@Sids-MacBook-Pro.local>
2017-09-16 16:25:40 -0400

    :)

--- findFlightAmadeus.py
+++ findFlightAmadeus.py
@@ -1,4 +1,4 @@
-from amadeus import Flights
+import requests
 import json
 
 apiKey = "ahylFqLEHHhLjtNh43BgIVwMHMMkWgmC"
@@ -6,10 +6,22 @@ origin = "NYC"
 departureDate = "2017-09-23"
 duration = 1
 maxPrice = 400
+period = "2016-09"
 
 priceDestinations = "https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search"
 topDestinations = "https://api.sandbox.amadeus.com/v1.2/travel-intelligence/top-destinations"
 
-flights = Flights(apiKey)
-resp = flights.inspiration_search(origin = origin, departure_date = departureDate, duration = duration, max_price = maxPrice)
-print json.dumps(resp)
+payloadPrice = {"apikey": apiKey, "origin": origin, "departure_date": departureDate, "duration": duration, "max_price": maxPrice}
+destinationList = requests.get(priceDestinations, params=payloadPrice)
+destinationList = destinationList.json()
+
+payloadTopDestinations = {"apikey": apiKey, 'period': period, 'origin': origin}
+topDestinationList = requests.get(topDestinations, params=payloadTopDestinations)
+topDestinationList = topDestinationList.json()
+
+for each in destinationList['results']:
+    print each['destination']
+
+from Queue import Queue
+for each in topDestinationList['results']:
+    if each['destination'] in
